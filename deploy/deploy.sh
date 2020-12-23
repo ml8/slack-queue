657ffb8543421e9b00d84dbfd67fea024cfd92e0
Matt Lang <langma@gmail.com>
2020-12-23 17:45:06 -0600

    add scripts to deploy container on GCP

---
+++ deploy/deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+gcloud compute instances create-with-container slack-queue \
+  --container-image gcr.io/${SQ_GCP_PROJECT}/slack-queue:latest \
+  --machine-type f1-micro \
+  --tags app \
+  --address ${SQ_GCP_IP} \
+  --disk name=slack-data-disk,mode=rw \
+  --container-mount-disk mount-path="/disks/data-disk",name=slack-data-disk,mode=rw \
+  --container-arg="-oauth=${SQ_OAUTH}" \
+  --container-arg="-ssecret=${SQ_SIGNING_SECRET}" \
+  --container-arg="-csecret=${SQ_CLIENT_SECRET}" \
+  --container-arg="-listCommand=list" \
+  --container-arg="-putCommand=tutorme" \
+  --container-arg="-takeCommand=tutor" \
+  --container-arg="-authChannel=faculty" \
+  --container-arg="-stateFilename=/disks/data-disk/qstate/state" \
+  --container-arg="-logtostderr"
+  
